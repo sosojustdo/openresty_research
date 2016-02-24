2e158edb7ae558de218376420127366b6a76f2b3
daipeng <daipeng@dangdang.com>
2016-02-24 14:26:14 +0800

    Signed-off-by: daipeng <daipeng.456@gmail.com>

--- openresty/lua/common.lua
+++ openresty/lua/common.lua
@@ -131,12 +131,31 @@ local function getToken()
 	return field
 end
 
+--split string
+local function lua_string_split(str, split_char)
+	if str == nil or split_char == nil then
+		return nil
+	end
+    local sub_str_tab = {};
+    while (true) do
+        local pos = string.find(str, split_char);
+        if (not pos) then
+            sub_str_tab[#sub_str_tab + 1] = str;
+            break;
+        end
+        local sub_str = string.sub(str, 1, pos - 1);
+        sub_str_tab[#sub_str_tab + 1] = sub_str;
+        str = string.sub(str, pos + 1, #str);
+    end
 
+    return sub_str_tab;
+end
 
 local _M = {
     read_http = read_http,
 	check_params = check_params,
 	getToken = getToken,
+	lua_string_split = lua_string_split,
 	common_tab = common_tab
 }
 
